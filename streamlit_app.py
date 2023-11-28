6f6bcd819da7c26e79861670b2db9ab7bf272d73
kasikarri <73255151+kasikarri@users.noreply.github.com>
2023-11-28 12:54:35 -0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,7 +8,6 @@ streamlit.text(' 🥣 Omega 3 and Blueberry Oatmeal')
 streamlit.text(' 🥗 Kale, Spinach and Rocket Smoothie')
 streamlit.text(' 🐔 Hard-Boiled Free-Range Egg')
 streamlit.text('🥑🍞 Avacado Toast')
-streamlit.text('Hello')
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
