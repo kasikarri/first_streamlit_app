ccfcec6d2b5f72093269a04e3fde9b775f15d03e
kasikarri <73255151+kasikarri@users.noreply.github.com>
2023-11-28 13:51:29 -0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -21,7 +21,9 @@ streamlit.dataframe(my_fruit_list)
 fruits_to_show = my_fruit_list.loc[fruits_selected]
 
 #streamlit.dataframe(fruits_selected)
-#streamlit.dataframe(fruits_to_show)
+
+# Display fruits selected
+streamlit.dataframe(fruits_to_show)
 
 
 
