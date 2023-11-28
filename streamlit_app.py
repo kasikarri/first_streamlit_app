853cf408d415258f1bee4bb513f0865948c72671
kasikarri <73255151+kasikarri@users.noreply.github.com>
2023-11-28 10:41:24 -0800

    Update streamlit_app.py
    Added Images

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,10 +2,10 @@ import streamlit
 streamlit.title('My Parents new healthy diner')
 
 streamlit.header('Breakfast Menu')
-streamlit.text('Omega 3 and Blueberry Oatmeal')
-streamlit.text('Kale, Spinach and Rocket Smoothie')
-streamlit.text('Hard-Boiled Free-Range Egg')
-
+streamlit.text(' 🥣 Omega 3 and Blueberry Oatmeal')
+streamlit.text(' 🥗 Kale, Spinach and Rocket Smoothie')
+streamlit.text(' 🐔 Hard-Boiled Free-Range Egg')
+streamlit.text('🥑🍞 Avacado Toast')
 
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
